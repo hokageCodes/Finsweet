8a02ac41a798b82e03e53660c3050805f5595fcd
hokageCodes <hashsenju94@gmail.com>
2023-04-25 23:53:51 +0100

    Dmm

---
+++ src/components/about/About.jsx
@@ -0,0 +1,20 @@
+import './about.css'
+
+function About() {
+    return (
+        <section className='about__section'>
+            <h4>ABOUT US</h4>
+            <div className="about__top">
+                <div className="about__top__left">
+                    <h2>The company leads entire webdesign process from concept to delivery.</h2>
+                </div>
+                <div className="about__top__right">
+                    <h3>The Era Of Technology</h3>
+                    <p>Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <nav className='nav__container'>
+            <h2>Party Deal</h2>
+            <div>
+                <ul className="nav__links">
+                    <li>Home</li>
+                    <li>Service</li>
+                    <li>Company</li>
+                    <li>Career</li>
+                    <li>Blog</li>
+                    <li>Contact Us</li>
+                    <div className="cta__link">
+                        <a href="/">Clone project</a>
+                        <HiOutlineArrowNarrowRight />
+                    </div>
+                </ul>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

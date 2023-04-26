5259bcd6bd583c5f425f3b7aa9074aaef1fa74d0
hokageCodes <hashsenju94@gmail.com>
2023-04-26 17:59:44 +0100

    im really really going this time

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,54 @@
 import './about.css'
+import Middle1 from '../../assets/middle_1.png'
+import Middle2 from '../../assets/middle_2.png'
+import Middle3 from '../../assets/middle_3.png'
+import Group from '../../assets/Group.png'
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
 
 function About() {
     return (
         <section className='about__section'>
-            <h4>ABOUT US</h4>
             <div className="about__top">
                 <div className="about__top__left">
+                    <h4>ABOUT US</h4>
                     <h2>The company leads entire webdesign process from concept to delivery.</h2>
                 </div>
                 <div className="about__top__right">
                     <h3>The Era Of Technology</h3>
                     <p>Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. </p>
+                </div>
+            </div>
+            <div className="about__middle">
+                <img src={Middle1} alt='1' />
+                <img src={Middle2} alt='' />
+                <img src={Middle3} alt='' />
+            </div>
+            <div className="about__bottom">
+                <div className="bottom-left">
+                    <div className="group">
+                        <p>1560+</p>
+                        <img src={Group} alt='' />
+                        <p>Projet Delivered</p>
+                    </div>
+                    <div className="group">
+                        <p>1560+</p>
+                        <img src={Group} alt='' />
+                        <p>Projet Delivered</p>
+                    </div>
+                    <div className="group">
+                        <p>1560+</p>
+                        <img src={Group} alt='' />
+                        <p>Projet Delivered</p>
+                    </div>
+                    <div className="group">
+                        <p>1560+</p>
+                        <img src={Group} alt='' />
+                        <p>Projet Delivered</p>
+                    </div>
+                </div>
+                <div className="bottom-right">
+                    <a href="/">Read about us</a>
+                    <HiOutlineArrowNarrowRight />
                 </div>
             </div>
         </section>

d3daf6fa50b3f7ef0460a8d0c6cb77d01ec6e7c6
hokageCodes <hashsenju94@gmail.com>
2023-04-30 05:15:37 +0100

    light

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,10 @@
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
 import Settings from '../../assets/settings.svg'
+import './services.css'
 
 function Services() {
     return (
-        <section>
+        <section className='services__container'>
             <div className="top">
                 <h4>OUR SERVICES</h4>
                 <h2>We build software solutions that solve clients business challenges</h2>
@@ -13,12 +14,12 @@ function Services() {
                 </div>
             </div>
             <div className="bottom">
-                <div className="card_arrangement">
+                <div className="card__arrangement">
                     <img src={Settings} alt="4" />
                     <h4>Technical support</h4>
                     <p>We aim to attain the greatest satisfaction for our clients and be one of the prominent names when it comes to world-class service</p>
                     <div className="bottom-right">
-                        <a href="/">Read about us</a>
+                        <a href="/">Read more</a>
                         <HiOutlineArrowNarrowRight />
                     </div>
                 </div>

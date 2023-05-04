5425997e697c556fb701608d057d1d0632a3cd7b
hokageCodes <hashsenju94@gmail.com>
2023-05-04 18:21:37 +0100

    yes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,11 @@ import Navbar from "../components/navbar/Navbar"
 import Hero from "../components/hero/Hero"
 import About from "../components/about/About"
 import Expertise from "../components/expertise/Expertise"
+import Services from "../components/services/Services"
+import Process from "../components/process/Process"
 // import Blog from "../components/blog&news/Blog"
 // import FooterTop from "../components/footer/FooterTop"
 // import Newsletter from "../components/newsletter/Newsletter"
-// import Process from "../components/process/Process"
-// import Services from "../components/services/Services"
 // import Testimonial from "../components/testimonial/Testimonial"
 
 function LandingPage() {
@@ -16,9 +16,9 @@ function LandingPage() {
             <Hero />
             <About />
             <Expertise />
-            {/* <Services />
+            <Services />
             <Process />
-            <Testimonial />
+            {/* <Testimonial />
             <Blog />
             <Newsletter />
             <FooterTop /> */}
